db577a8dd5893a33f96baaefd5e430bdac9b3f3f
DHRUVI TALA <dhruvitala1007@gmail.com>
2026-09-16 22:53:14 +0530

    Update primeNumber.py

--- primeNumber.py
+++ primeNumber.py
@@ -4,7 +4,7 @@ if n<2:
 else:
     for i in range(2,n):
         if n%i==0:
-            print("Prime")
+            print("Not Prime")
             break
         else:
-            print("not Prime")
+            print("Prime")
