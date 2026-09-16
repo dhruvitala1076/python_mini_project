cb53a73f979bd69660d7203395c722420e556325
DHRUVI TALA <dhruvitala1007@gmail.com>
2026-09-16 22:45:10 +0530

    Add files via upload

---
+++ primeNumber.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a Number:"))
+if n<2:
+    print("Not prime")
+else:
+    for i in range(2,n):
+        if n%i==0:
+            print("Prime")
+            break
+        else:
+            print("not Prime")
